fa8b636bb5b7f3e5c7cb5f312c7eddd5e91dcabe
Aspect231 <140119721+Aspect231@users.noreply.github.com>
2025-08-30 14:21:41 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `jobcore_xp` (
+	`identifier` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_uca1400_ai_ci',
+	`job` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_uca1400_ai_ci',
+	`xp` INT(11) NULL DEFAULT '0'
+)
+ENGINE=InnoDB
+;
